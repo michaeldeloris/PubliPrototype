d088c955f225227330b7e93dd86addaeeb681b18
michaeldeloris <michaeldeldev@gmail.com>
2020-04-04 23:01:14 +0200

    Init app + router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PublicationsComponent } from './publications/publications.component';
+
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {path: "", component: PublicationsComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    FooterComponent,
+    PublicationsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
