48dba5c217a40896867b1cad52276fd7374fcaad
michaeldeloris <michaeldeldev@gmail.com>
2020-04-10 11:14:38 +0200

    add logout btn + manage display of login/logout buttons

--- src/app/components/topbar/topbar.component.ts
+++ src/app/components/topbar/topbar.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+import { Subscription } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
   styleUrls: ['./topbar.component.less']
 })
-export class TopbarComponent implements OnInit {
+export class TopbarComponent implements OnInit, OnDestroy {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+              private authService: AuthService) { }
+
+  isLogged = false;
+
+  subscription: Subscription;
+
+  message = '';
 
   ngOnInit(): void {
+    this.subscription = this.authService.authenticationResultEvent.subscribe(
+      next => this.isLogged = this.authService.isAuthenticated
+    )
+    this.authService.checkIfAlreadyAuthenticated();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   accessHome() {
@@ -23,6 +41,11 @@ export class TopbarComponent implements OnInit {
 
   accesUsersEdit() {
     this.router.navigate(['users', 'edit']);
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['']);
   }
 
 }
