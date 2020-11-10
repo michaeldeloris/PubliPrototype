d13017f37d1e74341097e0665c8cfb13b6527da8
michaeldeloris <michaeldeldev@gmail.com>
2020-11-10 16:25:48 +0100

    add roles (enum)

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,11 +1,18 @@
 export class User {
   id: number;
   username: string;
+  role: Role;
 
   static fromHttp(user: User): User {
     const newUser = new User();
     newUser.id = user.id;
     newUser.username = user.username;
+    newUser.role = user.role;
     return newUser;
   }
+}
+
+export enum Role {
+  ADMIN = 'ADMIN',
+  USER = 'USER'
 }
