6a44035fea3737ed8ca23ff58ad7ab3c86efc744
michaeldeloris <michaeldeldev@gmail.com>
2020-11-13 14:43:04 +0100

    Init user detail component

--- src/app/users/user-detail/user-detail.component.ts
+++ src/app/users/user-detail/user-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/app/models/User';
+import { AuthService } from 'src/app/services/auth.service';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-user-detail',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDetailComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+
+  message: string;
+
+  constructor(private dataService: DataService,
+              private authService: AuthService,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.loadData();
+  }
+
+  loadData() {
+    const id = this.activatedRoute.snapshot.queryParams['id'];
+    this.dataService.getUsers().subscribe(
+      next => {
+        if(id) {
+          this.user = next.find(user => user.id === +id);
+        } else {
+          this.user = next.find(user => user.username === this.authService.username);
+        }
+      },
+      error => this.message = 'Connexion avec le serveur interrompue'
+    );
   }
 
 }
